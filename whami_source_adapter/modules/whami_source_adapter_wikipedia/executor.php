d7d8925548b24895ecef23cbf855259d1d907fc2
Pablo Lopez <plopesc@gmail.com>
2011-01-03 18:17:02 +0100

    Final proof of concept of the Whami Executor and Whami source Adapter modules

---
+++ whami_source_adapter/modules/whami_source_adapter_wikipedia/executor.php
@@ -0,0 +1,43 @@
+<?php
+
+//Change the current work directory
+ chdir($_GET['cwd']);
+
+
+include_once 'includes/bootstrap.inc';
+include_once 'includes/common.inc';
+include_once 'includes/module.inc';
+include_once 'includes/unicode.inc';
+//Check the existence or not of the mb_string extension
+unicode_check();
+
+
+drupal_bootstrap(DRUPAL_BOOTSTRAP_LANGUAGE);
+
+
+//Loading required modules
+
+drupal_load('module', 'system');
+
+drupal_load('module', 'whami_source_adapter');
+
+drupal_load('module', 'geonames');
+
+drupal_load('module', 'whami_source_adapter_wikipedia');
+
+//Loading module list 
+module_list(TRUE, FALSE);
+
+//Call to the sourceAdapter
+if($_GET['method'] == 'bbox'){
+  $bbox = array(
+    'north' => isset($_GET['north']) ? $_GET['north'] : 90,
+    'south' => isset($_GET['south']) ? $_GET['south'] : -90,
+    'east' => isset($_GET['east']) ? $_GET['east'] : 180,
+    'west' => isset($_GET['west']) ? $_GET['west'] : -180,
+  );
+  whami_get_information_bounding_box(NULL, $_GET['language'], $_GET['categoriesID'], $_GET['sourceID'], $_GET['maxRows'], $_GET['length_title'], $_GET['length_body'], $_GET['length_url'], $_GET['maxRequestRows'], $_GET['sessionID']);
+}else if($_GET['method'] == 'geopoint_radius'){
+  whami_get_information_geopoint_radius($_GET['lon'], $_GET['lat'], $_GET['radius'], $_GET['language'], $_GET['categoriesID'], $_GET['sourceID'], $_GET['maxRows'], $_GET['length_title'], $_GET['length_body'], $_GET['length_url'], $_GET['maxRequestRows'], $_GET['sessionID']);
+}
+exit();
